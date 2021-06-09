35b764bf526fc2ca8fb180a7dddb3c464051c7ac
Clint Laub <gwapolaub@gmail.com>
2021-06-09 11:05:30 -0600

    additional work

--- todo_complete.py
+++ todo_complete.py
@@ -111,7 +111,9 @@ darkPalette.setColor(QPalette.Base, QColor(42, 42, 42))
 darkPalette.setColor(QPalette.AlternateBase, QColor(66, 66, 66))
 darkPalette.setColor(QPalette.ToolTipBase, Qt.white)
 darkPalette.setColor(QPalette.ToolTipText, Qt.white)
-darkPalette.setColor(QPalette.Text, Qt.white)
+darkPalette.setColor(QPalette.Text, Qt.darkGray)
+
+#darkPalette.setColor(QPalette.QLineEdit.Text, Qt.white)
 darkPalette.setColor(QPalette.Disabled, QPalette.Text, QColor(127, 127, 127))
 darkPalette.setColor(QPalette.Dark, QColor(35, 35, 35))
 darkPalette.setColor(QPalette.Shadow, QColor(20, 20, 20))
@@ -125,7 +127,6 @@ darkPalette.setColor(QPalette.Disabled, QPalette.Highlight, QColor(80, 80, 80))
 darkPalette.setColor(QPalette.HighlightedText, Qt.white)
 darkPalette.setColor(QPalette.Disabled, QPalette.HighlightedText, QColor(127, 127, 127))
 app.setPalette(darkPalette)
-
 
 window.show()
 app.exec()
