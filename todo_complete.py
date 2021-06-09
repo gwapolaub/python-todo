a0dcee066c54fa8274f1a0a439c02a1f95ac519c
Clint Laub <gwapolaub@gmail.com>
2021-06-09 01:16:42 -0600

    3 element tuple additional workmore worGot 3 elements to show

--- todo_complete.py
+++ todo_complete.py
@@ -1,7 +1,10 @@
 import json
 import sys
+import datetime
 
 from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtGui import QPalette, QColor
+
 from PySide6.QtCore import Qt
 
 from MainWindow import Ui_MainWindow
@@ -48,7 +51,8 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     and then clearing it.
     """
     text = self.todoEdit.text()
-    number = self.todoEdit.text()
+    number = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     if text:  # Don't add empty strings.
       # Access the list via the model.
       self.model.todos.append((False, text, number))
@@ -98,5 +102,30 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
+
+darkPalette = QPalette()
+darkPalette.setColor(QPalette.Window, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.WindowText, Qt.white)
+darkPalette.setColor(QPalette.Disabled, QPalette.WindowText, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.Base, QColor(42, 42, 42))
+darkPalette.setColor(QPalette.AlternateBase, QColor(66, 66, 66))
+darkPalette.setColor(QPalette.ToolTipBase, Qt.white)
+darkPalette.setColor(QPalette.ToolTipText, Qt.white)
+darkPalette.setColor(QPalette.Text, Qt.white)
+darkPalette.setColor(QPalette.Disabled, QPalette.Text, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.Dark, QColor(35, 35, 35))
+darkPalette.setColor(QPalette.Shadow, QColor(20, 20, 20))
+darkPalette.setColor(QPalette.Button, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.ButtonText, QColor(53, 53, 53))
+darkPalette.setColor(QPalette.Disabled, QPalette.ButtonText, QColor(127, 127, 127))
+darkPalette.setColor(QPalette.BrightText, Qt.red)
+darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
+darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+darkPalette.setColor(QPalette.Disabled, QPalette.Highlight, QColor(80, 80, 80))
+darkPalette.setColor(QPalette.HighlightedText, Qt.white)
+darkPalette.setColor(QPalette.Disabled, QPalette.HighlightedText, QColor(127, 127, 127))
+app.setPalette(darkPalette)
+
+
 window.show()
 app.exec()
